8229ca8fae9252daf284e119daf7307e51026197
Mehmango <46484137+Mehmango@users.noreply.github.com>
2022-04-06 02:00:48 +0800

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,15 @@
+embed
+<drac2>
+args = &ARGS&
+
+title = ":levitate: Admins Commands :levitate:"
+
+desc = "❧ `!maze generate` ⟶ Randomly generates a new maze for the server and saves it as a server variable :arrows_clockwise: \n*(Only usable by members with the Dragonspeaker/Server Aliaser role)*"
+desc += "\n\n❧ `!maze display` ⟶ Displays a top down view of the maze :eyes:\n*(Should only be used to confirm the maze's validity, and not as part of the minigame)*"
+
+footer = "Maze alias by Mehmango\n!maze for help"
+
+image = "https://images.alphacoders.com/691/thumb-1920-691531.jpg"
+
+return f""" -title "{title}" -desc "{desc}" -footer "{footer}" -image "{image}" """
+</drac2>
